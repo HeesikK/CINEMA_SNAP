b23497c39b7937c63be9b42451fd03afa18d4e68
HeesikK <7jjhkhz@naver.com>
2024-05-23 10:44:52 +0900

    🟢 feat: global, common, theme 스타일 추가 및 적용

--- cinema/src/App.tsx
+++ cinema/src/App.tsx
@@ -1,9 +1,17 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
+import { ThemeProvider } from "styled-components";
+import theme from "./style/theme.style";
+import GlobalStyles from "./style/global.style";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
