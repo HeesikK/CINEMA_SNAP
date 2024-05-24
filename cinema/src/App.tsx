77157210de177c79cfdb05316a9eb9733c987d69
HeesikK <7jjhkhz@naver.com>
2024-05-24 09:47:20 +0900

    🟢 feat: react query 세팅 & 환경 변수 추가

--- cinema/src/App.tsx
+++ cinema/src/App.tsx
@@ -4,13 +4,20 @@ import router from "./router/router";
 import { ThemeProvider } from "styled-components";
 import theme from "./style/theme.style";
 import GlobalStyles from "./style/global.style";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {},
+  });
+
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
